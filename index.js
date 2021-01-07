c04f29f7942da5e3858eeb1b04a76e0243035110
Mike Mueller <mikemuellerware@gmail.com>
2021-01-07 11:09:05 -0500

    rename package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const { inputParser } = require('./src/inputParser/inputParser')
-const { formatValidTrips } = require('./src/tripFormatter/tripFormatter')
-const { reportBuilder } = require('./src/reportBuilder/reportBuilder')
+const { inputParser } = require('./src/input-parser/inputParser')
+const { formatValidTrips } = require('./src/trip-formatter/tripFormatter')
+const { reportBuilder } = require('./src/report-builder/reportBuilder')
 
 const data = fs.readFileSync(0)
 
